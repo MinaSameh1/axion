0a8e55c058da5b0604cec32e8956004a551bd11b
MinaSameh1 <minasameh1@gmail.com>
2024-02-13 02:05:02 +0200

    feat(school): Finish crud

--- managers/entities/school/School.manager.js
+++ managers/entities/school/School.manager.js
@@ -1,3 +1,5 @@
+const getSelfHandleResponse = require("../../api/_common/getSelfResponse");
+
 module.exports = class School {
   constructor({
     utils,
@@ -6,29 +8,53 @@ module.exports = class School {
     cortex,
     managers,
     validators,
-    mongomodels,
+    oyster,
   } = {}) {
     this.config = config;
     this.cortex = cortex;
     this.validators = validators;
     this.tokenManager = managers.token;
     this.shark = managers.shark;
+    this.oyster = oyster;
     this.responseDispatcher = managers.responseDispatcher;
     this._label = "schools";
-    this.schoolExposed = ["createSchool"];
+    this.schoolExposed = ["createSchool", "get=getSchool", "delete=deleteSchool", "put=updateSchool"];
   }
 
-  async createSchool({ name, address, phone, userId }) {
+  async #getUser({ userId }) {
+    const user = await this.oyster.call("get_block", `user:${userId}`);
+    if (!user || Object.keys(user).length === 0) {
+      return { error: "Invalid Token" };
+    }
+    return user;
+  }
+
+  async #checkPermission({ userId, role, action, nodeId = "board.school" }) {
+    const canDoAction = await this.shark.isGranted({
+      layer: "board.school",
+      action,
+      userId,
+      nodeId,
+      role,
+    });
+    return canDoAction;
+  }
+
+  async createSchool({ name, address, phone, __token, res }) {
+    const { userId } = __token;
+    const user = await this.#getUser({ userId });
+    if (user.error) return user;
+
+    if (!user.role) {
+      return { error: "User role not found" };
+    }
+
     // Permission check
-    const canCreateSchool = await this.shark.isGranted(
-      {
-        layer: "school",
-        action: "create",
-        userId,
-        nodeId: "board.school",
-        isOwner: false,
-      }
-    );
+    const canCreateSchool = await this.#checkPermission({
+      userId,
+      role: user.role,
+      action: "create",
+    });
 
     if (!canCreateSchool) {
       return { error: "Permission denied" };
@@ -36,17 +62,140 @@ module.exports = class School {
 
     const school = { name, address, phone };
 
-
     // Data validation
     let result = await this.validators.school.createSchool(school);
 
     if (result) return result;
 
     // Creation Logic
+    const createdSchool = await this.oyster.call("add_block", {
+      ...school,
+      _id: name,
+      _label: this._label,
+    });
+
+    if (createdSchool.error) {
+      if (createdSchool.error.includes("already exists")) {
+        this.responseDispatcher.dispatch(res, {
+          code: 409,
+          message: "Name already exists",
+        });
+        return getSelfHandleResponse();
+      }
+      console.error("Failed to create school", createdSchool.error);
+      this.responseDispatcher.dispatch(res, {
+        code: 500,
+        message: "Failed to create school",
+      });
+      return getSelfHandleResponse();
+    }
+
+    // Response
+    return { school: createdSchool };
+  }
+
+  async getSchool({ __query }) {
+    const { id } = __query;
+    // Data validation
+    let result = await this.validators.school.getSchool({ id });
+
+    if (result) return result;
+
+    const school = await this.oyster.call("get_block", id);
+    if (!school || Object.keys(school).length === 0) {
+      return { error: "School not found" };
+    }
+    return school;
+  }
+
+  async updateSchool({ id, name, address, phone, __token, res }) {
+    const { userId } = __token;
+    const user = await this.#getUser({ userId });
+    if (user.error) return user;
+
+    if (!user.role) {
+      return { error: "User role not found" };
+    }
+
+    // Permission check
+    const canUpdateSchool = await this.#checkPermission({
+      userId,
+      role: user.role,
+      action: "update",
+    });
+
+    if (!canUpdateSchool) {
+      return { error: "Permission denied" };
+    }
+
+    const school = { name, address, phone, id };
+
+    // Data validation
+    let result = await this.validators.school.updateSchool(school);
+
+    if (result) return result;
+
+    // Creation Logic
+    const createdSchool = await this.oyster.call("update_block", {
+      ...school,
+      _id: id,
+      _label: this._label,
+    });
+
+    if (createdSchool.error) {
+      console.error("Failed to update school", createdSchool.error);
+      this.responseDispatcher.dispatch(res, {
+        code: 500,
+        message: "Failed to update school",
+      });
+      return getSelfHandleResponse();
+    }
 
     // Response
     return {
       school: createdSchool,
     };
   }
+
+  async deleteSchool({ __token, __query, res }) {
+    const { userId } = __token;
+    const user = await this.#getUser({ userId });
+    if (user.error) return user;
+
+    if (!user.role) {
+      return { error: "User role not found" };
+    }
+
+    // Permission check
+    const canDeleteSchool = await this.#checkPermission({
+      userId,
+      role: user.role,
+      action: "delete",
+    });
+
+    if (!canDeleteSchool) {
+      return { error: "Permission denied" };
+    }
+
+    // Data validation 
+    let result = await this.validators.school.getSchool(__query);
+    if (result) return result;
+
+    const { id } = __query;
+    const school = await this.oyster.call("delete_block", id);
+    if (school.error) {
+      console.error("Failed to delete school", school.error);
+      this.responseDispatcher.dispatch(res, {
+        code: 500,
+        message: "Failed to delete school",
+      });
+      return getSelfHandleResponse();
+    }
+
+    // Response
+    return {
+      school,
+    };
+  }
+
 };
--- managers/entities/school/school.schema.js
+++ managers/entities/school/school.schema.js
@@ -1,9 +1,35 @@
-module.exports = {
-  createSchool: [
+const schoolSchema = [
+  {
+    model: "text",
+    path: "name",
+    required: true,
+  },
     {
       model: "text",
-      path: "name",
+      path: "address",
       required: true,
     },
+    {
+      model: "text",
+      path: "phone",
+      required: true,
+    },
+];
+
+const schoolIdSchema = [
+  {
+    model: "text",
+    path: "id",
+    required: true,
+  },
+];
+
+module.exports = {
+  createSchool: schoolSchema,
+  getSchool: schoolIdSchema,
+  updateSchool: [
+    ...schoolSchema,
+    ...schoolIdSchema,
   ],
+  deleteSchool: schoolIdSchema,
 };
